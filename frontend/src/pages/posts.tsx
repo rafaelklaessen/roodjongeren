0aaf0a4f916b2c94f612f96891f6be9193e0faf7
Rafael Klaessen <rafael@egodact.com>
2025-01-02 23:10:04 +0100

    fix #61 persists post search state

--- frontend/src/pages/posts.tsx
+++ frontend/src/pages/posts.tsx
@@ -1,15 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { fetchFallback, fetchPosts } from "../utils/backend";
 import { Post } from "../models/Post";
 import { StrapiPagination } from "../models/strapi";
 import CollectionViewer from "../components/CollectionViewer";
 import PostItem from "../components/PostItem";
-import { GetStaticPropsContext, GetStaticPropsResult } from "next";
+import { GetServerSidePropsContext, GetStaticPropsResult } from "next";
 import Banner from "../components/Banner";
 import Main from "../components/Main";
-import { revalidate } from "../utils/revalidate";
 import Search from "../components/Search";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import HeadPage from "../components/HeadPage";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
 
 const PAGE_SIZE = 16;
 
@@ -19,21 +21,96 @@ interface Props {
     pagination: StrapiPagination;
 }
 
+type InitialSearchState = {
+    searchString: string;
+    page: number;
+}
+
+const parseQueryPage = (page: unknown): number | null => {
+    if (typeof page !== 'string') {
+        return null;
+    }
+
+    const parsedPage = parseInt(page);
+
+    if (Number.isNaN(parsedPage)) {
+        return null;
+    }
+
+    return parsedPage;
+}
+
+const getInitialSearchState = (query: ParsedUrlQuery): InitialSearchState => {
+    const initialSearchState = {
+        searchString: "",
+        page: 1,
+    };
+   
+    if (typeof query.search === 'string') {
+        initialSearchState.searchString = query.search.trim();
+    }
+
+    const page = parseQueryPage(query.page);
+
+    if (page !== null) {
+        initialSearchState.page = page;
+    }
+
+    return initialSearchState;
+}
+
 export default function PostsPage(props: Props) {
+    const router = useRouter();
+    const initialSearchState = useMemo(
+        () => getInitialSearchState(router.query),
+        [],
+    );
+
     const [posts, setPosts] = useState(props.posts);
     const [pagination, setPagination] = useState(props.pagination);
     const [isLoading, setLoading] = useState(false);
-    const [searchString, setSearchString] = useState("");
-    const [page, setPage] = useState(1);
+    const [searchString, setSearchString] = useState(initialSearchState.searchString);
+    const [page, setPage] = useState(initialSearchState.page);
 
     const handleSearch = useCallback((newSearchString: string) => {
         setPage(1);
         setSearchString(newSearchString);
     }, []);
 
+    const syncSearchStateToQueryParams = (searchString: string, page: number): void => {
+        const queryParams = {
+            ...router.query,
+        };
+
+        delete queryParams.search;
+        delete queryParams.page;
+
+        if (searchString.length > 0) {
+            queryParams.search = searchString;
+        }
+
+        if (page > 1) {
+            queryParams.page = page.toString();
+        }
+
+        router.push(
+            {
+                pathname: router.pathname,
+                query: queryParams,
+            },
+            undefined,
+            {
+                shallow: true,
+            },
+        );
+    }
+
     useEffect(() => {
+        syncSearchStateToQueryParams(searchString, page);
+
         function retrievePosts() {
             setLoading(true);
+
             fetchPosts(null, null, null, page, PAGE_SIZE, null, searchString ?? null)
                 .then((it) => {
                     setPosts(it.posts);
@@ -44,13 +121,16 @@ export default function PostsPage(props: Props) {
                 });
         }
 
-        if (!searchString && page === 1) {
+        if (
+            searchString === initialSearchState.searchString &&
+            page === initialSearchState.page
+        ) {
             setPosts(props.posts);
             setPagination(props.pagination);
         } else {
             retrievePosts();
         }
-    }, [searchString, page, props]);
+    }, [searchString, page]);
 
     return (
         <div>
@@ -62,6 +142,7 @@ export default function PostsPage(props: Props) {
             <Banner title="Posts" background={props.pageBanner} compact />
             <Main className="container">
                 <Search
+                    initialSearchString={initialSearchState.searchString}
                     onChange={handleSearch}
                     placeholder="Zoek op titel, auteur of inhoud"
                     isLoading={isLoading}
@@ -82,16 +163,25 @@ export default function PostsPage(props: Props) {
     );
 }
 
-export async function getStaticProps(
-    context: GetStaticPropsContext
+export async function getServerSideProps(
+    context: GetServerSidePropsContext,
 ): Promise<GetStaticPropsResult<Props>> {
+    const initialSearchState = getInitialSearchState(context.query);
+
     const [{ posts, pagination }, { pageBanner }] = await Promise.all([
-        fetchPosts(null, null, null, 1, PAGE_SIZE),
+        fetchPosts(
+            null,
+            null,
+            null,
+            initialSearchState.page,
+            PAGE_SIZE,
+            null,
+            initialSearchState.searchString || null,
+        ),
         fetchFallback(),
     ]);
 
     return {
         props: { pageBanner, posts, pagination },
-        revalidate,
     };
 }
